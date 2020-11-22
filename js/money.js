2a38fa25fdf3123b97cd9eff3761592a05eba99a
ダウスケ <ksye616@gmail.com>
2020-11-22 19:07:42 +0900

    memo-commit2

--- js/money.js
+++ js/money.js
@@ -1,4 +1,6 @@
 $(function () {
+    // const db = firebase.firestore().collection('kadai04');
+
     //画面の切り替え
     $('.tab a').on('click', function () {
         $(this).parent().addClass('active').siblings('.active').removeClass('active');
@@ -7,51 +9,72 @@ $(function () {
         return false;
     });
     //撮影画面
-    window.onload = () => {
-        const video = document.querySelector("#camera");
-        const canvas = document.querySelector("#picture");
-        const se = document.querySelector('#se');
+    let accountval;         //勘定科目のvalue値取得用
+    let purchasedateval;    //購入日のvalue値取得用
 
-        //カメラ設定
-        const constraints = {
-            audio: false,
-            video: {
-                width: 300,
-                height: 200,
-                facingMode: "user"   // フロントカメラを利用する
-                // facingMode: { exact: "environment" }  // リアカメラを利用する場合
-            }
-        };
-
-        // カメラを<video>と同期
-        navigator.mediaDevices.getUserMedia(constraints)
-            .then((stream) => {
-                video.srcObject = stream;
-                video.onloadedmetadata = (e) => {
-                    video.play();
-                };
-            })
-            .catch((err) => {
-                console.log(err.name + ": " + err.message);
-            });
-
+    $('#select').change(function() {
+        // 選択されているvalue属性値を取り出す
+        accountval = $('#select').val();
+        console.log(accountval); 
+        //取得したvalue値を#accountitemsに出力
+        $('#accountitems').val(accountval);
+        return accountval;
+    });
+    $('#purchaseday').change(function () { 
+        purchasedateval = $('#purchaseday').val();
+        console.log(purchasedateval);
+        return purchasedateval;
+    });
+    $('#submit').on('click', function () {
+        const data = {
+        day: purchasedateval,
+        text: accountval,
+        memo: $('#textmemo').val(),
+    }
+    console.log(data);
+     });
+    
+    // db.orderBy('time', 'desc').onSnapshot(function (querySnapshot) {
+    //     const dataArray = []; 
+    //     querySnapshot.docs.forEach(function (doc) {
+    //     const data = {
+    //     id: doc.id,
+    //     data: doc.data(),
+    // }
+    // dataArray.push(data);
+    //     });
         
-        //シャッターボタン 
-        document.querySelector("#shutter").addEventListener("click", () => {
-            const ctx = canvas.getContext("2d");
-
-            // 演出的な目的で一度映像を止めてSEを再生する
-            video.pause();  // 映像を停止
-            se.play();      // シャッター音
-            setTimeout(() => {
-                video.play();    // 0.5秒後にカメラ再開
-            }, 500);
-
-            // canvasに画像を貼り付ける
-            ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    // const tagArray = []; 
+    // dataArray.forEach(function (data) {
+    // agArray.push(`
+    // <li id=${data.id}>
+    // <p>${data.data.name}</p>
+    // <p>${data.data.text}</p>
+    // <p>${data.data.time.seconds}</p>
+    // </li>
+    // `)
+    // })
+    // $('#output').html(tagArray);
+    // });
+    $('#upload_file').on('change', function(){ // ファイルが選択されるたびに動作するイベント
+        const strFileInfo = $('#upload_file')[0].files[0]; // ファイルオブジェクトを変数に格納
+        
+        // 選択されたファイルが画像であれば、処理を継続
+        if(strFileInfo && strFileInfo.type.match('image.*')){ 
+            $('#preview').remove();  // 表示中のプレビュー画像があれば、削除しておく
+            $('#preview_area').append('<img id="preview" width="400" />');  // 横幅400pxの空の画像をプレビューエリアにセット
+            fileReader = new FileReader(); // FileReader()メソッドを初期化
+            fileReader.onload = function(event){
+                $('#preview').attr('src', event.target.result);
+            }
+            fileReader.readAsDataURL(strFileInfo); // ローカルフォルダから画像ファイルを読込
+        }
+        $("#submit").click(function(){
+            const src = $(this).siblings('#preview_area').find('img').attr('src');
+            console.log(src);
         });
-    };
 
+    });
     //カレンダーの処理
     $('#datepicker').datepicker({
         showButtonPanel: true,      //カレンダー最下部に「Today」「Done」ボタンを追加
---
+++ readme.md
@@ -0,0 +1,2 @@
+#マネレコ
+デプロイ先：https://dausuke.github.io/04_13_maneleco/
