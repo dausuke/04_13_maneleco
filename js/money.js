a57f58a6c2fbcb4c9eb66b1ec68c38a115db68b2
ダウスケ <ksye616@gmail.com>
2020-11-23 17:05:04 +0900

    memo-commit4

--- js/money.js
+++ js/money.js
@@ -1,5 +1,6 @@
 $(function () {
-    // const db = firebase.firestore().collection('kadai04');
+
+    const db = firebase.firestore().collection('kadai04');
 
     //画面の切り替え
     $('.tab a').on('click', function () {
@@ -8,57 +9,14 @@ $(function () {
         $(content).addClass('active').siblings('.active').removeClass('active');
         return false;
     });
+
     //撮影画面
     let accountval;         //勘定科目のvalue値取得用
     let purchasedateval;    //購入日のvalue値取得用
+    let imgfile;            //画像のscr取得用
 
-    $('#select').change(function() {
-        // 選択されているvalue属性値を取り出す
-        accountval = $('#select').val();
-        console.log(accountval); 
-        //取得したvalue値を#accountitemsに出力
-        $('#accountitems').val(accountval);
-        return accountval;
-    });
-    $('#purchaseday').change(function () { 
-        purchasedateval = $('#purchaseday').val();
-        console.log(purchasedateval);
-        return purchasedateval;
-    });
-    $('#submit').on('click', function () {
-        const data = {
-        day: purchasedateval,
-        text: accountval,
-        memo: $('#textmemo').val(),
-    }
-    console.log(data);
-     });
-    
-    // db.orderBy('time', 'desc').onSnapshot(function (querySnapshot) {
-    //     const dataArray = []; 
-    //     querySnapshot.docs.forEach(function (doc) {
-    //     const data = {
-    //     id: doc.id,
-    //     data: doc.data(),
-    // }
-    // dataArray.push(data);
-    //     });
-        
-    // const tagArray = []; 
-    // dataArray.forEach(function (data) {
-    // agArray.push(`
-    // <li id=${data.id}>
-    // <p>${data.data.name}</p>
-    // <p>${data.data.text}</p>
-    // <p>${data.data.time.seconds}</p>
-    // </li>
-    // `)
-    // })
-    // $('#output').html(tagArray);
-    // });
-    $('#upload_file').on('change', function(){ // ファイルが選択されるたびに動作するイベント
+    $('#upload_file').on('change', function(){ // ファイルが選択されるたびに作動
         const strFileInfo = $('#upload_file')[0].files[0]; // ファイルオブジェクトを変数に格納
-        
         // 選択されたファイルが画像であれば、処理を継続
         if(strFileInfo && strFileInfo.type.match('image.*')){ 
             $('#preview').remove();  // 表示中のプレビュー画像があれば、削除しておく
@@ -69,15 +27,67 @@ $(function () {
             }
             fileReader.readAsDataURL(strFileInfo); // ローカルフォルダから画像ファイルを読込
         }
-        $("#submit").click(function(){
-            const src = $(this).siblings('#preview_area').find('img').attr('src');
-            console.log(src);
-        });
-
     });
+    
+    //#selectで選択された項目に応じて、alue値を#accountitemsに出力
+    $('#select').change(function() {
+        // 選択されているvalue属性値を取り出し、変数accountvalに格納
+        accountval = $('#select').val();
+        //取得したvalue値を#accountitemsに出力
+        $('#accountitems').val(accountval);
+        return accountval;
+    });
+
+    //#purchasedayで選択された日付を変数purchasedatevalに格納
+    $('#purchaseday').change(function () { 
+        purchasedateval = $('#purchaseday').val();
+        console.log(purchasedateval)
+        return purchasedateval;
+    });
+
+    //送信処理
+    $('#submit').on('click', function () {
+        imgfile = $(this).siblings('.imgform').find('#preview').attr('src');
+        const data = {
+            day: purchasedateval,
+            text: accountval,
+            memo: $('#textmemo').val(),
+            img: imgfile
+        }
+        db.add(data);
+     });
+    
+    // データをリアルタイムに取得する処理
+    db.onSnapshot(function (querySnapshot) {
+        const dataArray = [];
+        querySnapshot.docs.forEach(function (doc) {
+            const data = {
+                id : doc.id,
+                data : doc.data(),
+                };
+                dataArray.push(data);
+                // console.log(dataArray);
+        });
+        const tagArray = [];
+        dataArray.forEach(function (data) {
+            const tag = `
+                <li id = "${data.id}">
+                <p>${data.data.day}</p>
+                <p>${data.data.text}</p>
+                <img id="decodeimg" width="400" src= ${data.data.img} />
+                <p>${data.data.memo}</p>
+                </li>
+            `;
+            tagArray.push(tag);
+            });
+        $('.calendarmemo').html(tagArray);
+        });
+    
     //カレンダーの処理
-    $('#datepicker').datepicker({
-        showButtonPanel: true,      //カレンダー最下部に「Today」「Done」ボタンを追加
+    $('.datepicker').datepicker({
         dateFormat: 'yy/mm/dd',     //表示形式：年/月/日
+        onSelect: function(dateText, inst) {
+            console.log(dateText);
+        }
     });
 });
